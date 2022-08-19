282d38408ab52fb47a9fc09b997a5bdad6a02054
KajolT <109175336+KajolT@users.noreply.github.com>
2022-08-19 23:01:48 +0530

    Add files via upload

---
+++ Pandas_practice.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+df=pd.read_csv("McD_data.csv")
+print(df)
+
+
+# In[3]:
+
+
+df=read_csv('India_Menu.csv')
+print(df)
+
+
+# In[ ]:
+
+
+
+
